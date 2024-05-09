aa4cf195f1b51997944c0f860fce36bf8e78c61d
saatvik-10 <saatvikmadan409@gmail.com>
2024-05-09 21:23:09 +0530

    added user registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -14,6 +14,32 @@ const authUser = asyncHandler(async (req, res) => {
 //access- Public
 
 const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+
   res.status(200).json({ message: 'Register User' });
 });
 
